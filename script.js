afe45c332799b6da308d84f7696c1f10a8504519
Ullrickjohn <example@gnail.com>
2023-09-23 23:09:55 +0500

    feat complated navbar section

---
+++ script.js
@@ -0,0 +1,6 @@
+const Button = document.querySelector('Button')
+Button.addEventListener('click', function(){
+    Button.style.background = 'purple'
+})
+Button.addEventListener('dblclick', function(){
+    Button.style.background = 'green'})
